81718b634907d6d92ed3c7db4e312eb6fac239a0
deena <Deenaa@outlook.com>
2018-02-02 13:27:17 +0300

    working on my part

---
+++ app/src/main/java/com/example/deena/easybid/C_canceledR.java
@@ -0,0 +1,13 @@
+package com.example.deena.easybid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class C_canceledR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_canceled_r);
+    }
+}
---
+++ app/src/main/java/com/example/deena/easybid/C_incompleteR.java
@@ -0,0 +1,13 @@
+package com.example.deena.easybid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class C_incompleteR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_incomplete_r);
+    }
+}
